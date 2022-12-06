64d942bcbdf4635c4c7356be1c8dc7ae5d2cf164
Anastasiya <melihovaanastasya@yandex.ru>
2022-12-06 22:29:56 +0300

    80

--- ABC_BAC/Program.cs
+++ ABC_BAC/Program.cs
@@ -1,20 +1,20 @@
 ﻿// ABC=>CAB, ABC=>CBA
 
-логический тест (строка s1, строка s2)
+bool Test(string s1,string s2)
 {
     s1=s1.ToLower();
     s2=s2.ToLower();
-    если (s1.Length!=s2.Length) вернуть false;
+    if (s1.Length!=s2.Length) return false;
     char[] c1=s1.ToCharArray();
     char[] c2=s2.ToCharArray();
-    Массив.Сортировка(c1);//ABC
-    Массив.Сортировка(c2);//ABC
-    s1 = новая строка (c1);
-    s2 = новая строка (c2);
-    вернуть s1==s2;
+    Array.Sort(c1);//ABC
+    Array.Sort(c2);//ABC
+    s1=new String(c1);
+    s2=new String(c2);
+    return s1==s2;
 }
 
-строка s1="ABcDe";
-строка s2="EDbCa";
+string s1="ABcDe";
+string s2="EDbCa";
 
-System.Console.WriteLine(Тест(s1,s2));
+System.Console.WriteLine(Test(s1,s2));
