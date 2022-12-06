d3981fc1c26e6025074405d1ef5a5a707075ca03
Anastasiya <melihovaanastasya@yandex.ru>
2022-12-06 23:31:43 +0300

    84

---
+++ 84/Program.cs
@@ -0,0 +1,13 @@
+﻿// 84. Определить являются ли введенные с клавиатуры символы правильно записью целого числа. Вычислить сумму цифр введенного числа
+
+string s = Console.ReadLine();
+int k = 0;
+int s1 = 0;
+for (int i = 0; i < s.Length; i++)
+    if (char.IsDigit(s[i]))
+    {
+        k++;
+        s1 += s[i] - 48;
+    }
+if (k == s.Length) System.Console.WriteLine(s1);
+else System.Console.WriteLine("Не число");
