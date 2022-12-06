0e80ee254af4a42b3c5ee4c556540dea4e645276
Anastasiya <melihovaanastasya@yandex.ru>
2022-12-06 23:38:57 +0300

    86

---
+++ 86/Program.cs
@@ -0,0 +1,7 @@
+﻿// 86. Подсчитать сколько раз определенный символ (например 'a') встречается в строке.
+
+string s=Console.ReadLine();
+int k=0;
+for (int i=0;i<s.Length;i++)
+if (s[i]==(char)97) k++;
+System.Console.WriteLine(k);
