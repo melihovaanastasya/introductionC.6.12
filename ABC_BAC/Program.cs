fb85e696d7a4ed60cd7340ab19804f5ebb807f73
Anastasiya <melihovaanastasya@yandex.ru>
2022-12-06 22:02:53 +0300

    seminar

---
+++ ABC_BAC/Program.cs
@@ -0,0 +1,20 @@
+﻿// ABC=>CAB, ABC=>CBA
+
+логический тест (строка s1, строка s2)
+{
+    s1=s1.ToLower();
+    s2=s2.ToLower();
+    если (s1.Length!=s2.Length) вернуть false;
+    char[] c1=s1.ToCharArray();
+    char[] c2=s2.ToCharArray();
+    Массив.Сортировка(c1);//ABC
+    Массив.Сортировка(c2);//ABC
+    s1 = новая строка (c1);
+    s2 = новая строка (c2);
+    вернуть s1==s2;
+}
+
+строка s1="ABcDe";
+строка s2="EDbCa";
+
+System.Console.WriteLine(Тест(s1,s2));
---
+++ symbol/Program.cs
@@ -0,0 +1,12 @@
+﻿// с помощью System.Linq;
+
+символ с='а';
+с='\u0064';
+символ б = 'а';
+System.Console.WriteLine((int)b);
+System.Console.WriteLine(c.ToString()+b.ToString());
+if (b>='0' && b<='9') System.Console.WriteLine("Это цифра");
+if (b>='A' && b<='Z') System.Console.WriteLine("Это большая латиница");
+if (b>='a' && b<='z' || b>='A' && b<='Z') System.Console.WriteLine("Это большая латиница");
+
+if (Char.IsDigit(b)) System.Console.WriteLine("Это цифра");
